eb84457cb8898069ef3f9ebf4e32144488f3c8b5
SolomonVP <Lum7023@yandex.ru>
2023-07-10 22:42:41 +0300

    init

---
+++ app/src/main/java/com/demo/firstkotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.demo.firstkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonSave.setOnClickListener { textViewName.text = "Привет, ${editTextName.text.toString().trim()}!" }
+    }
+}
